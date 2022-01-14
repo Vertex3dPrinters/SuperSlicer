26493f125988e12c3b6ba938964e2f728103aaf1
supermerill <merill@free.fr>
2022-01-14 16:44:10 +0100

    tries macosbuild on macos11

--- deps/OpenVDB/OpenVDB.cmake
+++ deps/OpenVDB/OpenVDB.cmake
@@ -7,27 +7,9 @@ else()
 endif()
 
 if (APPLE)
-    message(STATUS "Using apple path for openvdb ${CMAKE_STATIC_LINKER_FLAGS}")
     find_package(zstd REQUIRED)
-	# set(CMAKE_STATIC_LINKER_FLAGS "${CMAKE_STATIC_LINKER_FLAGS} -v -Xlinker -v -v")
-	# set(CMAKE_SHARED_LINKER_FLAGS "${CMAKE_SHARED_LINKER_FLAGS} -v -Xlinker -v -v")
-	# # set(CMAKE_SHARED_LINKER_FLAGS "-Xlinker -v -v")
-	# # set(CMAKE_MODULE_LINKER_FLAGS "-Xlinker -v -v")
-	# # set(CMAKE_EXE_LINKER_FLAGS "-Xlinker -v -v")
-	# # set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -stdlib=libc++ -v")
-    # message(STATUS "Using apple path for openvdb1 ${CMAKE_STATIC_LINKER_FLAGS}")
-    # message(STATUS "Using apple path for openvdb2 ${LDFLAGS}")
-	# -DCMAKE_SHARED_LINKER_FLAGS="Wl, -v"
-	execute_process(
-        COMMAND brew --prefix zstd
-        RESULT_VARIABLE BREW_ZSTD
-        OUTPUT_VARIABLE BREW_ZSTD_PREFIX
-        OUTPUT_STRIP_TRAILING_WHITESPACE
-    )
-	message(STATUS "BREW_ZSTD=${BREW_ZSTD}")
-	message(STATUS "BREW_ZSTD_PREFIX=${BREW_ZSTD_PREFIX}")
-	message(STATUS "OUTPUT_STRIP_TRAILING_WHITESPACE=${OUTPUT_STRIP_TRAILING_WHITESPACE}")
 endif()
+
 if(APPLE AND NOT BUILD_SHARED_LIBS)
 	prusaslicer_add_cmake_project(OpenVDB
 		URL https://github.com/tamasmeszaros/openvdb/archive/refs/tags/v6.2.1-prusa3d.zip #v6.2.1 patched
@@ -41,7 +23,7 @@ if(APPLE AND NOT BUILD_SHARED_LIBS)
 			-DOPENVDB_CORE_STATIC=${_build_static}
 			-DOPENVDB_ENABLE_RPATH:BOOL=OFF
 			-DTBB_STATIC=${_build_static}
-			-DOPENVDB_BUILD_VDB_PRINT=OFF
+			-DOPENVDB_BUILD_VDB_PRINT=ON
 			-DDISABLE_DEPENDENCY_VERSION_CHECKS=ON # Centos6 has old zlib
 #			-DCMAKE_STATIC_LINKER_FLAGS="${BREW_ZSTD_PREFIX}/lib/libzstd.a"
 	)
